75cdc8114693e4891fb7d0e6328e54cfb90d5751
Mario <mariosarasa.val@gmail.com>
2025-01-03 12:03:24 +0100

    1

---
+++ Final/main.py
@@ -0,0 +1,10 @@
+from module import interface 
+
+def main(): 
+    matrix = interface.create_3d_scene()
+    print("Final Rotation Matrix:", matrix)
+
+
+
+if __name__ == "__main__":
+    main()
--- Final/module/interface.py
+++ Final/module/interface.py
@@ -1,26 +1,20 @@
-# Librerías de interfaz gráfica
+
 import tkinter as tk
+
 from tkinter import messagebox, simpledialog
 
-# Librerías para gráficos
-import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
 from matplotlib.figure import Figure
 
-# Librerías de cálculo y manipulación de datos
 import numpy as np
-import pandas as pd
+
 from math import radians, sin, cos
-import itertools
-import os
 
-# Librerías para visualización 3D
 from vpython import radians, cos, sin
-from vpython import *  # Ten cuidado con el uso de `*`, puede causar conflictos.
 
-# Librerías para perfilado del código
-import cProfile
-import pstats
+from vpython import *  
+
 
 
 # Variable global para almacenar la matriz final
@@ -197,3 +191,5 @@ def create_3d_scene():
     root.mainloop()
 
     return final_rotation
+
+final_rotation = final_rotation 
